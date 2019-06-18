be580cd70d319cd35999247bf17118ff63a06522
borislavatblagoev <borislavatblagoev@gmail.com>
2019-06-18 20:55:32 +0300

    HouseOfCats

---
+++ HouseOfCats/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HouseOfCats
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("How many legs are there in the house?");
+            int legsInHouse = int.Parse(Console.ReadLine());
+
+            //Guarantee that the value is correct.
+            while (!(legsInHouse >= 0 && legsInHouse < 50) || legsInHouse % 2 != 0)
+            {
+                Console.WriteLine("Please enter valid value: ");
+                legsInHouse = int.Parse(Console.ReadLine());
+            }
+
+            //Invoke the method and put it in an array.
+            int[] possibleNumberOfHumansInHouse = CalculateNumberOfHumans(legsInHouse);
+
+            Console.WriteLine("The possible number of humans in the house.");
+            //Prints the possible number of humans in the house.
+            foreach (var numberOfHumans in possibleNumberOfHumansInHouse)
+            {
+                Console.Write($"{numberOfHumans}, ");
+            }
+            Console.ReadLine();
+
+        }
+
+        //Method to calculate the number of people in the house.
+        public static int[] CalculateNumberOfHumans(int legsInHouse)
+        {
+            int currentNumberOfHumans;
+
+            List<int> possibleNumberOfHumans = new List<int>();
+
+            while (legsInHouse >= 0)
+            {
+                currentNumberOfHumans = legsInHouse / 2;
+                possibleNumberOfHumans.Add(currentNumberOfHumans);
+                legsInHouse -= 4;
+            }
+
+            possibleNumberOfHumans.Reverse();
+            return possibleNumberOfHumans.ToArray();
+        }
+
+    }
+}
