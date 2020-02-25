5a16c4aa5b90e00e4b742809317e9e8f4fa63d1a
borislavatblagoev <borislavatblagoev@gmail.com>
2020-02-25 12:00:54 +0200

    asd

--- HouseOfCats/Program.cs
+++ HouseOfCats/Program.cs
@@ -8,7 +8,7 @@ namespace HouseOfCats
         static void Main(string[] args)
         {
 
-            //alo new 
+            //alo new asd
             Console.WriteLine("How many legs are there in the house?");
             int legsInHouse = int.Parse(Console.ReadLine());
 
